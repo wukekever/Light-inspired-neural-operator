62e265a70d9dee6e9fa6af8baf1f096b704adb77
wukekever <wukekever@gmail.com>
2026-06-02 22:46:14 +0800

    Update files

--- scripts/run_train_deeponet_burgers1d.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-## Working directory: Light-inspired-neural-operator
-## Usage: bash ./scripts/run_train_deeponet_burgers1d.sh
-
-set -euo pipefail
-
-cd "$(dirname "$0")/.." && cd src
-
-python run_deeponet_burgers1d.py
